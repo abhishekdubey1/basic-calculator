b4152b6039c0a71fd89af91431dc9dc259601044
ad99526 <Abhishekk>
2020-08-05 17:03:53 +0530

    Updates

---
+++ precache-manifest.c9e102108d9efa4962a87e17dfece2b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adcb2f33e9c5e665193ad4c1aa71eebf",
+    "url": "/basic-calculator/index.html"
+  },
+  {
+    "revision": "06a640a38548ced92ebc",
+    "url": "/basic-calculator/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "14bb76ae1c44981ee3c3",
+    "url": "/basic-calculator/static/js/2.2602f5cc.chunk.js"
+  },
+  {
+    "revision": "06a640a38548ced92ebc",
+    "url": "/basic-calculator/static/js/main.3b51d62c.chunk.js"
+  },
+  {
+    "revision": "ae4278509112aba0f296",
+    "url": "/basic-calculator/static/js/runtime~main.1353636e.js"
+  }
+]);
